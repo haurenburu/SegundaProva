1bbea5cb6000b931f434ff109b10acc31955a744
haurenburu <haurenburu@protonmail.com>
2021-04-06 18:08:04 -0300

    ADD: navigation to cadastra frag

--- app/src/main/java/com/example/segundaprova/HomeFragment.kt
+++ app/src/main/java/com/example/segundaprova/HomeFragment.kt
@@ -2,12 +2,13 @@ package com.example.segundaprova
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import com.example.segundaprova.databinding.FragmentHomeBinding
@@ -15,13 +16,6 @@ import com.example.segundaprova.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
     lateinit var viewmodel: HomeFragViewModel
-
-
-//    private val db: AppDatabase by lazy {
-//        Room.databaseBuilder(parentFragment?.context!!, AppDatabase::class.java, "games.sqlite")
-//            .allowMainThreadQueries()
-//            .build()
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,12 +32,20 @@ class HomeFragment : Fragment() {
             adapter.notifyDataSetChanged()
         })
 
-        //adapter.games = db.gameDao().listAll()
+        setHasOptionsMenu(true)
 
-
-        val layout = LinearLayoutManager(parentFragment?.context!!, LinearLayoutManager.VERTICAL, false)
+        val layout = LinearLayoutManager(parentFragment?.requireContext(), LinearLayoutManager.VERTICAL, false)
         binding.recyclerGameList.layoutManager = layout
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, this.findNavController()) || super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/segundaprova/MainActivity.kt
+++ app/src/main/java/com/example/segundaprova/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.segundaprova
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
@@ -15,21 +17,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val game1 = Game("nome1", "diretor1", "desenvolvedor1", "editora1", 2020, 2)
-//        val game2 = Game("nome2", "diretor2", "desenvolvedor2", "editora2", 2020, 3)
-//        val game3 = Game("nome3", "diretor3", "desenvolvedor3", "editora3", 2020, 4)
-//        val game4 = Game("nome4", "diretor4", "desenvolvedor4", "editora4", 2020, 5)
-//
-//        with(db.gameDao()){
-//            insert(game1)
-//            insert(game2)
-//            insert(game3)
-//            insert(game4)
-//        }
-
-//        val gl:MutableList<Game> = db.gameDao().listAll();
-//        val adapter = GameAdapter()
-//        adapter.games = gl
     }
 }
