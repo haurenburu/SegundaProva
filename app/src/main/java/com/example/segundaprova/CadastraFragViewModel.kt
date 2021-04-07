9383b9d23015bab718a65a8e1a98074b6e3e3794
haurenburu <haurenburu@protonmail.com>
2021-04-07 17:02:28 -0300

    ADD: create game
    TODO: need to update the recycler on create

--- app/src/main/java/com/example/segundaprova/CadastraFragViewModel.kt
+++ app/src/main/java/com/example/segundaprova/CadastraFragViewModel.kt
@@ -1,12 +1,47 @@
 package com.example.segundaprova
 
-import androidx.lifecycle.ViewModel
+import android.annotation.SuppressLint
+import android.app.Application
+import android.os.AsyncTask
+import androidx.lifecycle.AndroidViewModel
+import androidx.room.Room
 
-class CadastraFragViewModel: ViewModel() {
+class CadastraFragViewModel(application: Application) : AndroidViewModel(application) {
+    private var dba: AppDatabase
+
+    init {
+        val db:AppDatabase by lazy {
+            Room.databaseBuilder(application.applicationContext,
+                AppDatabase::class.java, "games.sqlite")
+                .build()
+        }
+        dba = db
+    }
+
     var name: String = ""
     var director: String = ""
     var developer: String = ""
     var publisher: String = ""
     var releaseYear: Int = 0
     var rating: Int = 0
+
+    fun createGame() {
+        CreateGameAsync().execute()
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private inner class CreateGameAsync() : AsyncTask<Unit, Unit, Unit>() {
+        override fun doInBackground(vararg params: Unit?) {
+            val g = Game(
+                name,
+                director,
+                developer,
+                publisher,
+                releaseYear,
+                rating
+            )
+
+            dba.gameDao().insert(g)
+        }
+    }
 }
--- app/src/main/java/com/example/segundaprova/CadastraFragment.kt
+++ app/src/main/java/com/example/segundaprova/CadastraFragment.kt
@@ -6,16 +6,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
+import com.example.segundaprova.databinding.FragmentCadastraBinding
 
 class CadastraFragment : Fragment() {
     lateinit var viewmodel: CadastraFragViewModel
+    lateinit var binding: FragmentCadastraBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cadastra, container, false)
         viewmodel = ViewModelProvider(this).get(CadastraFragViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_cadastra, container, false)
+
+        binding.editTextName.addTextChangedListener {
+            viewmodel.name = binding.editTextName.text.toString()
+        }
+
+        binding.editTextDirector.addTextChangedListener {
+            viewmodel.director = binding.editTextDirector.text.toString()
+        }
+        binding.editTextDeveloper.addTextChangedListener {
+            viewmodel.developer = binding.editTextDeveloper.text.toString()
+        }
+        binding.editTextPublisher.addTextChangedListener {
+            viewmodel.publisher = binding.editTextPublisher.text.toString()
+        }
+        binding.editTextReleaseYear.addTextChangedListener {
+            viewmodel.releaseYear = binding.editTextReleaseYear.text.toString().toInt()
+        }
+        binding.editTextRating.addTextChangedListener {
+            viewmodel.releaseYear = binding.editTextRating.text.toString().toInt()
+        }
+
+        binding.button.setOnClickListener {
+            viewmodel.createGame()
+//            val g = Game(
+//                viewmodel.name,
+//                viewmodel.director,
+//                viewmodel.developer,
+//                viewmodel.publisher,
+//                viewmodel.releaseYear,
+//                viewmodel.rating
+//            )
+        }
+
+        return binding.root
     }
 }
