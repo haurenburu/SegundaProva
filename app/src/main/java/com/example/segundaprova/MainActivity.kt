a4555cf49adeb95b0be6543bfb4979d2e14d04ec
haurenburu <haurenburu@protonmail.com>
2021-04-08 12:53:59 -0300

    ADD: navigation up on bar

--- app/src/main/java/com/example/segundaprova/MainActivity.kt
+++ app/src/main/java/com/example/segundaprova/MainActivity.kt
@@ -8,14 +8,22 @@ import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
 
-    private val db: AppDatabase by lazy {
-        Room.databaseBuilder(this, AppDatabase::class.java, "games.sqlite")
-            .allowMainThreadQueries()
-            .build()
-    }
+//    private val db: AppDatabase by lazy {
+//        Room.databaseBuilder(this, AppDatabase::class.java, "games.sqlite")
+//            .allowMainThreadQueries()
+//            .build()
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.NavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.NavHostFragment)
+        return navController.navigateUp()
     }
 }
